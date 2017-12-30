2f4631793c4dbaf221b7343c93f22c29020619a2
thekinglaolu <afolayan.david7@gmail.com>
2017-12-30 16:48:05 +0100

    deployed final poject for the #ALCwithGoogle WB Track program

---
+++ projects/Day 21 - Pixel Art Maker/README.md
@@ -0,0 +1,15 @@
+# Pixel Art Maker Project
+
+## Author
+David 'LaOlu' Afolayan
+
+## About Project
+This is a single-page web app that allows users to draw pixel art on a customizable canvas.
+
+## Technologies Used
+* HTML
+* CSS
+* JavaScript (jQuery)
+
+## And he's aLive!!!
+[Pixel Art Maker](https://tkl-pixel-art-maker.herokuapp.com/pixelArt.html)
---
+++ projects/Day 21 - Pixel Art Maker/designs.js
@@ -0,0 +1,48 @@
+/********************************
+ Credit To NevehAllon For The Sample Code on CodePen
+ https://codepen.io/CodePenUserName/pen/NwzNEM?editors=0010
+ ********************************/
+
+//Makes the grid by listening to the number values in input.
+$(function(){
+	function makeGrid() {
+	  const row = $("#input_height").val();
+	  const column = $("#input_width").val();
+
+	  //takes values and uses them to make the apropriate grid
+	  for (let i = 0; i < row; i++) {
+	    $("#pixel_canvas").append("<tr></tr>");
+	    for (let j = 0; j < column; j++) {
+	      $("tr")
+	        .last()
+	        .append("<td></td>");
+	    };
+	  };
+	};
+
+	myColor = $("#colorPicker").val(); //listens and pulls color value
+
+	//listens for click and inserts color value to css
+	let click = $("#pixel_canvas").on("click", "td", function() {
+	  let myColor = $("#colorPicker").val();
+	  $(this).css("background-color", myColor);
+	});
+
+	//listens to the submit button and calls makeGrid function
+	let submit;
+	submit = $("#sizePicker").submit(function() {
+	  makeGrid();
+	  return false;
+	});
+
+	//Resets the design canvas as it clears the grid
+	let reset;
+	reset = $("#reset_button").click(function(e){
+	  e.preventDefault;
+	  $("tr").remove();
+	  $("td").remove();
+	  $("#input_height").val(1);
+	  $("#input_width").val(1);
+	});
+
+});
